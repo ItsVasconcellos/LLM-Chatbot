6552218614083fe8e7e2c9bbc8627c8266c6c510
ItsVasconcellos <fernando.asv@uol.com.br>
2024-12-10 11:32:30 -0300

    feat: adding initial steps to use ollama

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
 import streamlit as st 
+from llama_index.llms.ollama import Ollama
+from llama_index.core import Settings
+
+Settings.llm = Ollama(model="llama2", request_timeout=50.0)
 
 st.write("Bem vindo ao chatobt gênio da lâmpada!")
 st.text_input("Tire sua dúvida aqui!", key="doubt")
+
+if st.button("Enviar"):
+    st.write("Pensando...")
+    # response = Settings.llm.query(st.session_state.doubt)
