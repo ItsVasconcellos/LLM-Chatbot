aa15c2a96e467c6245348ca112dcf4f37a2398d5
ItsVasconcellos <fernando.asv@uol.com.br>
2024-12-11 20:00:11 -0300

    feat: adding rag and querying to streamlit

--- src/app.py
+++ src/app.py
@@ -1,22 +1,46 @@
 import streamlit as st 
-from llama_index.llms.ollama import Ollama
-from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
+
+import os
+from dotenv import load_dotenv
+
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, Settings, load_index_from_storage
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.node_parser import TokenTextSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.gemini import Gemini
 
-Settings.llm = Ollama(model="llama2", request_timeout=50.0)
+load_dotenv()
 
-# Correct the argument for HuggingFaceEmbedding
-Settings.embed_model = HuggingFaceEmbedding(model_name='sentence-transformers/all-MiniLM-L6-v2')
+GOOGLE_API_KEY = os.getenv("GEMINI_KEY")
+PERSIST_DIR = "./tmp"
 
-documents = SimpleDirectoryReader("./data").load_data()
+if not os.path.exists(PERSIST_DIR):
+    documents = SimpleDirectoryReader("./data").load_data()
 
-vector_index = VectorStoreIndex.from_documents(documents)
+    llm  = Gemini(model="models/gemini-1.5-pro", temperature=0.3, top_p=1, top_k=32, api_key=GOOGLE_API_KEY)
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    text_splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
+    splitter = TokenTextSplitter(chunk_size=1024, chunk_overlap=20)
 
-vector_index.as_query_engine()
+    # global settings
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+    Settings.text_splitter = text_splitter
+
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+
+else:
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+query_engine = index.as_query_engine()
 
 st.write("Bem vindo ao chatobt gênio da lâmpada!")
 st.text_input("Tire sua dúvida aqui!", key="doubt")
 
 if st.button("Enviar"):
     st.write("Pensando...")
-#     # response = Settings.llm.query(st.session_state.doubt)
+    response = query_engine.query(st.session_state.doubt)
+    st.write(response.response)
