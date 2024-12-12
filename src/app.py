2da6a44429bdd810fc0b4c6eabeb8d718e5b4b43
ItsVasconcellos <fernando.asv@uol.com.br>
2024-12-12 11:37:15 -0300

    feat: resolving error when model already exists

--- src/app.py
+++ src/app.py
@@ -14,20 +14,17 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("GEMINI_KEY")
 PERSIST_DIR = "./tmp"
 
-if not os.path.exists(PERSIST_DIR):
+llm = Gemini(model="models/gemini-1.5-pro", temperature=0.3, top_p=1, top_k=32, api_key=GOOGLE_API_KEY)
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+text_splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
+
+# global settings
+Settings.llm = llm
+Settings.embed_model = embed_model
+Settings.text_splitter = text_splitter
+
+if not os.path.exists(PERSIST_DIR):    
     documents = SimpleDirectoryReader("./data").load_data()
-
-    llm  = Gemini(model="models/gemini-1.5-pro", temperature=0.3, top_p=1, top_k=32, api_key=GOOGLE_API_KEY)
-    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-    text_splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
-    splitter = TokenTextSplitter(chunk_size=1024, chunk_overlap=20)
-
-    # global settings
-    Settings.llm = llm
-    Settings.embed_model = embed_model
-    Settings.text_splitter = text_splitter
-
-    documents = SimpleDirectoryReader("data").load_data()
     index = VectorStoreIndex.from_documents(documents)
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 
@@ -35,12 +32,17 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
     index = load_index_from_storage(storage_context)
 
-query_engine = index.as_query_engine()
+chat_engine = index.as_query_engine(similarity_top_k=2,system_prompt=("Consider that you are an assistnat made"   "to respond about industrial safety standards accodring to the context provided. "
+        "You shall give clear and concise answers based on the information provided in the context. "
+        "If there is no match betweenthe context and the question, you should respond that the answer is not known."
+        "Limit your response to 3 sentences. If really necessary, you can provide up to 5 sentences. "
+        "If you need more information, ask the user for clarification. "
+        "Always respond in English.\n\n"))
 
 st.title("Welcome to the master of engineering!")
 st.text_input("Ask any question and the genius will answer it for you!", key="doubt")
 
 if st.button("Ask the genius"):
     with st.spinner("Processing the data..."):
-        response = query_engine.query(st.session_state.doubt)
+        response = chat_engine.query(st.session_state.doubt)
     st.write("Result: " + response.response)
